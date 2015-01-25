942eb84e52fc06a2452b8143f6b980990bfe07ab
microprocessorguy <microprocessorguy@gmail.com>
2015-01-25 13:33:56 -0800

    prog. assgt #3 code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+###############################
+## Krish Sundaresan
+## 25-JAN-2015
+###############################
+# makeCachematrix() -- Creates a list containing fuctions to get/set the matrix and its inverse using solve()
+# cacheSolve() -- Calculates the inverse of a matrix using makeCachematrix() by using the cached values as appopriate
+#
+# Example usage:
+#
+# > mat <- matrix(rnorm(1024),nrow=32,ncol=32,byrow=TRUE)
+# > cachemat <- makeCacheMatrix(mat)
+# > cachemat$get() 
+# > cacheSolve(cachemat)
+# > cacheSolve(cachemat)
+# getting cached data
+# [,1]        [,2]        [,3]        [,4]        [,5]         [,6]
+# [1,]  0.037167890  0.20516323  0.01652198  0.01438210  0.01231357 -0.172866788
 
-## Write a short comment describing this function
 
+## Creates a list containing functions to get/set the matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    myinv <- NULL  # variable to hold inverse
+    
+    # set function for matrix
+    set <- function(y) {
+        x <<- y
+        myinv <<- NULL
+    }
+    
+    # get function for matrix
+    get <- function() x
+    
+    # set func for the inverse
+    setmatinv <- function(inverse) myinv <<- inverse
+    # get func for the inverse
+    getmatinv <- function() myinv
+    
+    # list to return
+    list(set = set, get = get,
+         setmatinv = setmatinv,
+         getmatinv = getmatinv)  
 }
 
 
-## Write a short comment describing this function
-
+## Calculates the inverse of a matrix using makeCachematrix() by using the cached values as appopriate
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    myinv <- x$getmatinv()
+    # Return the value if the inverse is already available
+    if(!is.null(myinv)) {
+        message("getting cached data")
+        return(myinv)
+    }
+    # else calculate it using solve()
+    data <- x$get()
+    myinv <- solve(data, ...)
+    
+    x$setmatinv(myinv)  # cache inverse for future
+    myinv  # return the inverse
 }
